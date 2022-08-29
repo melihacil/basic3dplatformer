3b68284fb7938eb291e2bbee0eab70f23aa8c3fc
melihacil <knghtmellomello@gmail.com>
2022-08-29 13:21:26 +0300

    Adding gamemanager

---
+++ My project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);  
+    }
+    public void ExitApp()
+    {
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}

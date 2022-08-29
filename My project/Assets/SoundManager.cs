6601bf73ecb6b966f636054d8cc04e77e31e4169
melihacil <knghtmellomello@gmail.com>
2022-08-29 13:50:41 +0300

    Adding sound manager

---
+++ My project/Assets/SoundManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    //For playing sounds
+    //Need to add main menu music and level music (can be the same music)
+}

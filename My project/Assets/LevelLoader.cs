75a175052cac0ea9981ab7eb35572296636c29f6
melihacil <knghtmellomello@gmail.com>
2022-08-29 13:34:06 +0300

    Updating game manager

---
+++ My project/Assets/LevelLoader.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+
+            float progress = Mathf.Clamp01((operation.progress / 9);
+            Debug.Log(progress);
+            yield return null;
+        }
+
+    }
+ 
+
+}
--- My project/Assets/Scripts/GameManager.cs
+++ My project/Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
+
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);  
@@ -17,7 +18,32 @@ public class GameManager : MonoBehaviour
 
     public void LoadNextLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        LoadLevel(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 
+
+
+    public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+
+            float progress = Mathf.Clamp01(operation.progress / 9);
+            Debug.Log(progress);
+            yield return null;
+        }
+
+    }
 }

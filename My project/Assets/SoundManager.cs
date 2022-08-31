fadf8d18a5baa3bd58b86988287a71b532ce8762
melihacil <knghtmellomello@gmail.com>
2022-08-31 11:53:11 +0300

    Updating sound manager

--- My project/Assets/SoundManager.cs
+++ My project/Assets/SoundManager.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+
+    public AudioClip[] clips;
+    
+    
     //For playing sounds
+    
     //Need to add main menu music and level music (can be the same music)
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        
+    }
+
+
+
+
+    
 }

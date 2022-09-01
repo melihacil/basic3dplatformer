9fa300b9512b49e3f18dbbdb665318ede0e50b88
melihacil <knghtmellomello@gmail.com>
2022-09-01 11:51:25 +0300

    Update ShootBomb.cs

--- My project/Assets/ShootBomb.cs
+++ My project/Assets/ShootBomb.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class ShootBomb : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public Transform shootingPoint;
+
+    public GameObject bomb;
+
+    private ObjectPool bombPool;
+
+
+
+    private void Awake()
     {
-        
+        bombPool = GetComponent<ObjectPool>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Start()
     {
-        
+        bombPool.Initialize(bomb, 5);
+    }
+
+    public void Fire()
+    {
+
+        var bomb = bombPool.CreateObject();
+
+        Rigidbody rb = bomb.GetComponent<Rigidbody>();
+
+        rb.velocity = new Vector3(0, 0, 0);
+        // Can be made scaling with distance !! Vector3.Distance(transform.position, player.position);
+        rb.AddForce(shootingPoint.forward * 6f, ForceMode.Impulse);
+        rb.AddForce(shootingPoint.up * 8f, ForceMode.Impulse);
     }
 }

5f712f3072ced773dfa856e36831b437ab298ee3
melihacil <knghtmellomello@gmail.com>
2022-09-01 12:39:31 +0300

    Update BossScript.cs

--- My project/Assets/BossScript.cs
+++ My project/Assets/BossScript.cs
@@ -30,14 +30,22 @@ public class BossScript : MonoBehaviour
     public bool playerInSightRange, playerInAttackRange;
     // Start is called before the first frame update
 
+
+    public GameObject projectile;
+
+    private ObjectPool objectPool;
+
+
+
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+        objectPool = GetComponent<ObjectPool>();
     }
     void Start()
     {
         rb.freezeRotation = true;
-
+        objectPool.Initialize(projectile, 2);
     }
 
     // Update is called once per frame
@@ -129,13 +137,27 @@ public class BossScript : MonoBehaviour
     {
         agent.SetDestination(transform.position);
         //transform.LookAt(player);
+
+        //transform.LookAt(player);
+
         if (!alreadyAttacked)
-        {         
+        {
+
+
+                Debug.Log("Attacking");
+                //Rigidbody rb = Instantiate(projectile, shootingPoint.position, Quaternion.identity).GetComponent<Rigidbody>();
+
+            var bomb = objectPool.CreateObject();
+            bomb.transform.position = shootingPoint.position;
+            bomb.transform.rotation = shootingPoint.rotation;
+            Rigidbody rb = bomb.GetComponent<Rigidbody>();
+            rb.velocity = new Vector3(0, 0, 0);
+            // Can be made scaling with distance !! Vector3.Distance(transform.position, player.position);
+            rb.AddForce(shootingPoint.forward * 6f, ForceMode.Impulse);
+            rb.AddForce(shootingPoint.up * 8f, ForceMode.Impulse);
             alreadyAttacked = true;
             Invoke(nameof(ResetAttack), timeBetweenAttacks);
         }
-
-
 
     }
 

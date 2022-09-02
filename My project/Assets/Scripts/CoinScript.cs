7b5763f180bc332a19257c5f9c5aa7b864db3d8e
melihacil <knghtmellomello@gmail.com>
2022-09-02 10:57:25 +0300

    Update CoinScript.cs

--- My project/Assets/Scripts/CoinScript.cs
+++ My project/Assets/Scripts/CoinScript.cs
@@ -7,12 +7,8 @@ public class CoinScript : MonoBehaviour
     [SerializeField]
     private int coinValue = 1;
 
-    private void Update()
-    {
-        //Need to add rotation to coin this does not work
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, new Quaternion (0,0,0,0), 30);
-    }
-    // Start is called before the first frame update
+    //Added coin animation
+   
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
